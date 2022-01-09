61d45bf2a1a844fd7e268d4ca5e499d33ba7d66e
rainbowemoji <heroskyweb@gmail.com>
2022-01-10 08:53:59 +0900

    fix autoplay to true

--- src/pages/JoinStream/index.tsx
+++ src/pages/JoinStream/index.tsx
@@ -84,10 +84,10 @@ export default function JoinStream({ metamaskProvider }) {
   return (
     <>
       <Header metamaskProvider={metamaskProvider} />
-      <div className="relative bg-event bg-cover bg-center rounded-xl w-2/3 m-auto">
+      <div className='relative bg-event bg-cover bg-center rounded-xl w-2/3 m-auto'>
         {loading ? (
-          <div className="w-full h-full m-auto pt-[56.25%] relative">
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className='w-full h-full m-auto pt-[56.25%] relative'>
+            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
               <BeatLoader loading={loading} />
             </div>
           </div>
@@ -95,33 +95,32 @@ export default function JoinStream({ metamaskProvider }) {
           <ReactHlsPlayer
             playerRef={playerRef}
             src={playbackURL}
-            autoPlay={false}
+            autoPlay={true}
             controls={isActive}
             playsInline
-            className="rounded-xl w-full h-full"
+            className='rounded-xl w-full h-full'
           />
         ) : (
-          <div className="w-full h-full m-auto pt-[56.25%] relative">
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-red-700 bg-white opacity-50 rounded-xl">
+          <div className='w-full h-full m-auto pt-[56.25%] relative'>
+            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-bold text-red-700 bg-white opacity-50 rounded-xl'>
               This event isn`t started now.
               <br />
               This will be removed when event is started.
             </div>
           </div>
         )}
-        <div className="absolute top-[90%] w-full left-1/2 transform -translate-x-1/2 -translate-y-1/2 grid grid-cols-6 items-center">
-          <div className="col-end-7 col-span-1">
+        <div className='absolute top-[90%] w-full left-1/2 transform -translate-x-1/2 -translate-y-1/2 grid grid-cols-6 items-center'>
+          <div className='col-end-7 col-span-1'>
             <button onClick={handleChat}>
               <svg
-                width="37"
-                height="35"
-                viewBox="0 0 37 35"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+                width='37'
+                height='35'
+                viewBox='0 0 37 35'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'>
                 <path
-                  d="M18.5 32.6875C28.7166 32.6875 37 25.4401 37 16.5C37 7.55988 28.7166 0.3125 18.5 0.3125C8.28337 0.3125 0 7.55988 0 16.5C0 20.57 1.71819 24.2931 4.55562 27.1375C4.33131 29.487 3.59131 32.0631 2.77269 33.9964C2.59 34.4265 2.94381 34.9075 3.404 34.8335C8.621 33.9779 11.7221 32.6644 13.0702 31.9799C14.8414 32.4523 16.6669 32.6902 18.5 32.6875Z"
-                  fill="#FFE6EB"
+                  d='M18.5 32.6875C28.7166 32.6875 37 25.4401 37 16.5C37 7.55988 28.7166 0.3125 18.5 0.3125C8.28337 0.3125 0 7.55988 0 16.5C0 20.57 1.71819 24.2931 4.55562 27.1375C4.33131 29.487 3.59131 32.0631 2.77269 33.9964C2.59 34.4265 2.94381 34.9075 3.404 34.8335C8.621 33.9779 11.7221 32.6644 13.0702 31.9799C14.8414 32.4523 16.6669 32.6902 18.5 32.6875Z'
+                  fill='#FFE6EB'
                 />
               </svg>
             </button>
@@ -136,7 +135,7 @@ export default function JoinStream({ metamaskProvider }) {
           />
         ) : null}
       </div>
-      <EventDescription event={event} stylec="w-3/4 mx-auto" />
+      <EventDescription event={event} stylec='w-3/4 mx-auto' />
     </>
   );
 }
